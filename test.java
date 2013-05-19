9daa871da5bb198c4abe1383aa58d6c6b710a1de
danxiangjie <wenzhibinbin_pt@163.com>
2013-05-19 20:53:39 +0800

    Update test.java

--- test.java
+++ test.java
@@ -3,6 +3,8 @@ package com.pt
    public static void main(String [] args){
     
        int  i=0;
-        System.out.println("I love you "+i);
+       
+      int   j=1;
+      System.out.println("I love you "+i);
    
  }
